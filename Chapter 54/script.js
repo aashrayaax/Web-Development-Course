5b0495ac50d9b79a4f5b3d5496810c490cdb01e5
aashrayaax <aashraya.basva.com>
2025-04-24 18:09:32 +0530

    completed

---
+++ Chapter 54/script.js
@@ -0,0 +1,21 @@
+// alert("Hi Welocme to JavaScript");
+
+console.log("Hello World");
+console.log("Hello World 2");
+console.log("Hello World 3");       
+console.log("Hello World 4");
+
+console.error("This is an error message");
+
+var a = prompt("Enter Your Number dude : ");
+console.log("Your number is : " + a);
+
+if(a>18){
+    console.log("You are an adult");
+}
+else if(a<18 && a>0){
+    console.log("You are a minor");
+}
+else if(a<0){
+    console.log("You are a baby");
+}
