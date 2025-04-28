9b15fef5cf271dfe00f9fc530613c60cd8273281
aashrayaax <aashraya.basva.com>
2025-04-28 15:15:01 +0530

    completed

---
+++ Chapter 60/index.js
@@ -0,0 +1,24 @@
+let a = "Aashraya";
+let b = "Harry Bhai"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+
+console.log(a.length)
+
+console.log(a.toLowerCase())
+console.log(a.toUpperCase())
+
+console.log(a.slice(1,4))
+console.log(b.trim("     "))
+
+console.log(`I am ${a} and my friend name is ${b}`)
+console.log("My name is " + a + " and my friend name is " + b)
+
+console.log(a.concat(b))
+console.log(a.replace("shr","hey"))
+
+
+// strings are immutable
+console.log(a)
+console.log(b)
